42567419770ab302c2946dc49a58afbbe21ad4d7
Nitin Gupta <75748537+Nitin-0205@users.noreply.github.com>
2023-04-15 13:49:33 +0530

    2

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/'>
+        <Route>
           <Header />
           <Home />
           <About />
